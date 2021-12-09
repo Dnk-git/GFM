23f14a7b2872a264576b5f3b04993fee8240cc68
Dnk-git <93070719+Dnk-git@users.noreply.github.com>
2021-12-09 17:28:10 +0500

    Add main files

---
+++ Tasks/Task_51/init.js
@@ -0,0 +1,17 @@
+
+import { addTask } from "./main.js";
+import {PRIORITY_LO, PRIORITY_HI} from "./main.js";
+
+addTask(PRIORITY_HI, `Вот вам и супер интересная тема.
+    Вы наверняка заметили что ваши файлы с кодом становятся все объемнее,
+    что хочется вынести некоторые вещи куда-то за пределы основной программы.`);
+
+addTask(PRIORITY_HI, `Сверстать этот TODO list`);
+
+addTask(PRIORITY_HI, `Начать делать задачу wewerwewewewe
+    ewrwerwerwerwerwewwwerwerwrrrrrrqqqqqqqqqqqqqqqqqqqrr`);
+
+addTask(PRIORITY_LO, `Посмотреть ютубчик`);
+
+addTask(PRIORITY_LO, `Посмотреть ютубчик 2 раза`);
+
---
+++ Tasks/Task_51/view.js
@@ -0,0 +1,10 @@
+
+export const UI_ELEMENTS = {
+    HI_INPUT: document.getElementById("hi_task_input"),
+    LO_INPUT: document.getElementById("lo_task_input"),
+    HI_INPUT_ADD_ICON: document.querySelector(".hi_input .add_task_icon"),
+    LO_INPUT_ADD_ICON: document.querySelector(".lo_input .add_task_icon"),
+    HI_ZONE_TASK: document.querySelector(".hi_zone"),
+    LO_ZONE_TASK: document.querySelector(".lo_zone")
+}
+
