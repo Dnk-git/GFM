33a96830decc81ea974016217489335ec61db223
Dnk-git <93070719+Dnk-git@users.noreply.github.com>
2021-12-14 21:27:32 +0500

    Add main files

---
+++ Tasks/Task_60/main_LS.js
@@ -0,0 +1,88 @@
+let countID = 0;
+
+const taskItem = {
+    ID: null,
+    name: '',
+    priority: '',
+    status: '',
+}
+let taskSet = [];
+
+const prioritySet = {
+    HI: 'high',
+    MD: 'middle',
+    LO: 'low',
+}
+
+const statusSet = {
+    TODO: 'To-Do',
+    INDO: 'Processing',
+    DONE: 'Done',
+}
+
+function addTaskToSet(name, priority, status){
+    let task = JSON.parse(JSON.stringify(taskItem));
+    countID++
+    task.ID = countID;
+    task.name = name;
+    task.priority = priority;
+    task.status = status;
+    taskSet.push(task);
+}
+
+function clearSessionStore(){
+    localStorage.clear();
+}
+
+function initTaskSet(){
+    addTaskToSet('Сделать задание с LocalStorage', prioritySet.HI, statusSet.TODO);
+    addTaskToSet('Повторить Try-Catch-Finally', prioritySet.MD, statusSet.TODO);
+    addTaskToSet('Порефаторить Vertical String - Task N...?', prioritySet.LO, statusSet.TODO);
+}
+
+function saveTasksToSessionStore(){
+    clearSessionStore();
+
+    let key = 0;
+    let item = '';
+    taskSet.forEach(element => {
+        key++;
+        item = JSON.stringify(element);
+        localStorage.setItem(key.toString(), item);
+    });
+}
+
+function restoreTasksFromSessionStore(){
+    taskSet.length = 0;
+    
+    let key = undefined;
+    const lengthStore = localStorage.length;
+    for (let i=0; i<lengthStore; i++){
+        key = localStorage.key(i);
+        let item = JSON.parse(localStorage.getItem(key));
+        console.log('Item of array: ' + item);
+        taskSet.push(item);
+    }    
+}
+
+function sortTaskSetByID(){
+    console.log('Before Sorting...');
+    console.dir(taskSet);
+
+    taskSet = taskSet.sort( (a, b) => (+a.ID - +b.ID) );
+    console.log('After Sorting...');
+    console.dir(taskSet);
+} 
+
+
+initTaskSet();
+console.dir(taskSet);
+saveTasksToSessionStore();
+console.dir(taskSet);
+restoreTasksFromSessionStore();
+console.dir(taskSet);
+sortTaskSetByID();
+console.dir(taskSet);
+
+
+
